daad26bb297f64e82e0cd3756f3e5247a356e480
conoroshea1996 <conoshea12@gmail.com>
2019-07-15 10:04:15 +0100

    add abilty to write and read messages

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+messages = []
+
+
+def add_messages(username, message):
+    """ Add message to messages list """
+    messages.append('{}: {}'.format(username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and seprate them with a br"""
+    return '<br>'.join(messages)
 
 
 @app.route('/')
@@ -12,12 +24,15 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return 'Hi ' + username
+    """ Display chat messages """
+    return '<h1>welcome, {} </h1> {}'.format(username, get_all_messages())
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return '{}:{}'.format(username, message)
+    """Create a new message and redirect back to chat """
+    add_messages(username, message)
+    return redirect('/' + username)
 
 
 app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
