f86251a821182a50eee2ba1add9c9dd7d5589da8
conoroshea1996 <conoshea12@gmail.com>
2019-07-15 09:23:27 +0100

    add routes

--- run.py
+++ run.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello mate</h1>'
+    """Main page with insturctions """
+    return 'To send a meesage use /USERNAME/MESSAGE'
+
+
+@app.route('/<username>')
+def user(username):
+    return 'Hi ' + username
+
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return '{}:{}'.format(username, message)
 
 
 app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
